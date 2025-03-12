63a842e615f6b7dc8180f4101257566da8f573a7
23IT101 POPAT HEMANGI <152361000+23it101PopatHemangi@users.noreply.github.com>
2025-03-13 04:33:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToDoList from './ToDoList';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+     <u><h1>To-Do List App</h1></u>
+      <ToDoList />
+    </div>
+  );
+};
+
+export default App;
